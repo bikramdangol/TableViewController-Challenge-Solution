6b18dbf06b7c02f9719a41934b048977459d84f5
Bikram Dangol <bdangol@Bikram-Ds-MacBook-Pro-2.local>
2015-07-31 18:33:24 -0600

    Initial Commit

---
+++ UITableViewController Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Bikram Dangol on 7/31/15.
+//  Copyright (c) 2015 Bikram Dangol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Bikram Dangol on 7/31/15.
+//  Copyright (c) 2015 Bikram Dangol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
