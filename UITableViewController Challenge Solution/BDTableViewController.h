82da93dcf440f792d8c09d491e948b62642ad4e4
Bikram Dangol <bdangol@Bikram-Ds-MacBook-Pro-2.local>
2015-07-31 19:38:16 -0600

    Solved the challenge

---
+++ UITableViewController Challenge Solution/BDTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BDTableViewController.h
+//  
+//
+//  Created by Bikram Dangol on 7/31/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BDTableViewController : UITableViewController
+
+@end
